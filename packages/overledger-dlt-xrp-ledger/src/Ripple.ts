88a69fe6f8f1e7834583e444406dbc5c67ddf46a
lukerQuant <luke.riley@quant.network>
2021-10-14 17:40:39 +0100

    lint issues

--- packages/overledger-dlt-xrp-ledger/src/Ripple.ts
+++ packages/overledger-dlt-xrp-ledger/src/Ripple.ts
@@ -12,7 +12,7 @@ import log4js from 'log4js';
 */
 const log = log4js.getLogger('Ripple');
 const elliptic = require('elliptic');
-const Secp256k1 = elliptic.ec('secp256k1');
+const secp256k1 = elliptic.ec('secp256k1');
 log.level = 'info';
 class Ripple extends AbstractDLT {
   rippleAPI: RippleAPI;
@@ -66,52 +66,51 @@ class Ripple extends AbstractDLT {
    *
    * @param {Account} accountInfo The standardised account information
    */
- setAccount(accountInfo: Account): void {
-  if (typeof accountInfo.privateKey === 'undefined') {
-    throw "accountInfo.privateKey must be set";
-  }
-  let thisPrivateKey = "";
-  let thisAddress = "";
-  let thisPublicKey = "";
-  let thisProvider = "";
-  let thisPassword = "";
-  let keypair;
+  setAccount(accountInfo: Account): void {
+    if (typeof accountInfo.privateKey === 'undefined') {
+      throw 'accountInfo.privateKey must be set';
+    }
+    let thisPrivateKey = '';
+    let thisAddress = '';
+    let thisPublicKey = '';
+    let thisProvider = '';
+    let thisPassword = '';
+    let keypair;
 
-  if (this.isValidSeed(accountInfo.privateKey)) {
-    log.info("Using seed to generate keypair.")
-    keypair = deriveKeypair(accountInfo.privateKey);
-  } else {
-    log.info("Using privateKey to generate publicKey.");
-    let privateKey = this.formatPrivateKey(accountInfo.privateKey);
-    const publicKey = bytesToHex(Secp256k1.keyFromPrivate(privateKey.slice(2)).getPublic().encodeCompressed());
-    this.validateKeyPair(privateKey, publicKey);
-    keypair = { privateKey, publicKey };
-  }
+    if (this.isValidSeed(accountInfo.privateKey)) {
+      log.info('Using seed to set account.');
+      keypair = deriveKeypair(accountInfo.privateKey);
+    } else {
+      log.info('Using privateKey to se account.');
+      const privateKey = this.formatPrivateKey(accountInfo.privateKey);
+      const publicKey = bytesToHex(secp256k1.keyFromPrivate(privateKey.slice(2)).getPublic().encodeCompressed());
+      this.validateKeyPair(privateKey, publicKey);
+      keypair = { privateKey, publicKey };
+    }
 
-  const generatedAddress = deriveAddress(keypair.publicKey);
-  thisPrivateKey = keypair.privateKey;
-  thisPublicKey = keypair.publicKey;
-  thisAddress = generatedAddress;
-  if ((typeof accountInfo.provider !== 'undefined')) {
-    thisProvider = accountInfo.provider;
-  } else {
-    thisProvider = "";
+    const generatedAddress = deriveAddress(keypair.publicKey);
+    thisPrivateKey = keypair.privateKey;
+    thisPublicKey = keypair.publicKey;
+    thisAddress = generatedAddress;
+    if ((typeof accountInfo.provider !== 'undefined')) {
+      thisProvider = accountInfo.provider;
+    } else {
+      thisProvider = '';
+    }
+    if ((typeof accountInfo.password !== 'undefined')) {
+      thisPassword = accountInfo.password;
+    } else {
+      thisPassword = '';
+    }
+    const thisAccount = {
+      privateKey: thisPrivateKey,
+      address: thisAddress,
+      publicKey: thisPublicKey,
+      provider: thisProvider,
+      password: thisPassword,
+    };
+    this.account = thisAccount;
   }
-  if ((typeof accountInfo.password !== 'undefined')) {
-    thisPassword = accountInfo.password;
-  } else {
-    thisPassword = "";
-  }
-  let thisAccount = {
-    privateKey: thisPrivateKey,
-    address: thisAddress,
-    publicKey: thisPublicKey,
-    provider: thisProvider,
-    password: thisPassword,
-  }
-  this.account = thisAccount;
-}
-
 
   sign(unsignedTransaction: PreparedTransaction): Promise<string> {
 
@@ -119,7 +118,7 @@ class Ripple extends AbstractDLT {
 
     const signedData = this.rippleAPI.sign(JSON.stringify(transactionData), {
       privateKey: this.account.privateKey,
-      publicKey: this.account.publicKey
+      publicKey: this.account.publicKey,
     });
     return Promise.resolve(signedData.signedTransaction);
   }
@@ -134,7 +133,7 @@ class Ripple extends AbstractDLT {
     } catch (err) {
       return false;
     }
-  };
+  }
 
   formatPrivateKey(privateKey: string): string {
     const prefix = '00';
@@ -147,7 +146,7 @@ class Ripple extends AbstractDLT {
     const signature = sign(messageToVerify, privateKey);
 
     /* istanbul ignore if */
-    if (verify(messageToVerify, signature, publicKey) !== true) {
+    if (!verify(messageToVerify, signature, publicKey)) {
       throw new Error('derived keypair did not generate verifiable signature');
     }
     return true;
