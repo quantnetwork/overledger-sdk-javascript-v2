3b52c898f6f714462b8a8bae3f09141b597b8734
lukerQuant <luke.riley@quant.network>
2021-10-15 12:25:21 +0100

    xrp-ledger test minor improvement

--- packages/overledger-dlt-xrp-ledger/tests/dlt.test.ts
+++ packages/overledger-dlt-xrp-ledger/tests/dlt.test.ts
@@ -4,18 +4,19 @@ import { DltNameOptions } from '@quantnetwork/overledger-types';
 describe('Dlt', () => {
     test('Can construct the DLT', () => {
         const mockAddress = 'rJ9qPMfCdFFpvXDYxjEcWDRPabNBYKkm3M'
-        const mockPrivateKey = 'ssy454BXBNCGmeUnqB3zvnG56ArCm';
-
+        const mockSeed = 'ssy454BXBNCGmeUnqB3zvnG56ArCm';
+        const mockPrivateKeyVersionOfSeed = '000FEA7F693B30168E216DA364DC45A0D6D18E0D84AFC1A2D4C9150BC8CA688B2A';
         const sdkOptions = {
             dlts: [{ dlt: DltNameOptions.XRP_LEDGER },],
         };
 
         const sdk = new OverledgerSDK(sdkOptions);
         sdk.dlts[DltNameOptions.XRP_LEDGER].setAccount({
-            privateKey: mockPrivateKey,
+            privateKey: mockSeed,
             address: mockAddress,
         },);
 
         expect(sdk.dlts[DltNameOptions.XRP_LEDGER].name).toEqual(DltNameOptions.XRP_LEDGER);
+        expect(sdk.dlts[DltNameOptions.XRP_LEDGER].account.privateKey).toEqual(mockPrivateKeyVersionOfSeed);
     });
 });
