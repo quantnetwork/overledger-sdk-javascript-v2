7177dc5e778b6377dbbc38dbbdc3df2855430489
lukerQuant <luke.riley@quant.network>
2021-10-21 10:04:17 +0100

    setting up refresh token test correctly

--- packages/overledger-aws-provider/test/aws.test.ts
+++ packages/overledger-aws-provider/test/aws.test.ts
@@ -50,24 +50,30 @@ describe('Integration Tests:', () => {
             console.log("Running full token test 2");
             //run tests using github secrets - see github repo
             const overledger = new OverledgerSDK({
-                dlts: [{ dlt: DltNameOptions.BITCOIN }, { dlt: DltNameOptions.ETHEREUM }, { dlt: DltNameOptions.XRP_LEDGER }],
-                provider: { network: 'https://auth.overledger.io/' },
+                dlts: [{ dlt: DltNameOptions.BITCOIN },
+                { dlt: DltNameOptions.ETHEREUM },
+                { dlt: DltNameOptions.XRP_LEDGER }],
+                userPoolID: 'us-east-1_xfjNg5Nv9', //your userpool id
+                provider: { network: 'https://auth.overledger.io/' }
             });
-            const refreshTokensResponse = await overledger.refreshAccessToken(process.env.CLIENT_ID, process.env.CLIENT_SECRET, refreshToken);
+            const refreshTokensResponse1 = await overledger.getTokensUsingClientIdAndSecret(process.env.USER_NAME, process.env.PASSWORD,
+                process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+
+            const refreshTokensResponse2 = await overledger.refreshAccessToken(process.env.CLIENT_ID, process.env.CLIENT_SECRET, refreshTokensResponse1.refreshToken);
     
             // access token is variable in length but must have at least 3 sections split by .
-            expect(typeof refreshTokensResponse.accessToken).toBe('string');
-            expect(refreshTokensResponse.accessToken.length).toBeGreaterThan(4);
-            expect(refreshTokensResponse.accessToken.split(".").length).toEqual(3);
+            expect(typeof refreshTokensResponse2.accessToken).toBe('string');
+            expect(refreshTokensResponse2.accessToken.length).toBeGreaterThan(4);
+            expect(refreshTokensResponse2.accessToken.split(".").length).toEqual(3);
             // id token is variable in length but must have at least 3 sections split by .
-            expect(typeof refreshTokensResponse.idToken).toBe('string');
-            expect(refreshTokensResponse.idToken.length).toBeGreaterThan(4);
-            expect(refreshTokensResponse.idToken.split(".").length).toEqual(3);
+            expect(typeof refreshTokensResponse2.idToken).toBe('string');
+            expect(refreshTokensResponse2.idToken.length).toBeGreaterThan(4);
+            expect(refreshTokensResponse2.idToken.split(".").length).toEqual(3);
             // upon refresh, token will be given an expiry
-            expect(typeof refreshTokensResponse.expiresIn).toBe('number');
-            expect(typeof refreshTokensResponse.expiresIn).toBeGreaterThan(0);
+            expect(typeof refreshTokensResponse2.expiresIn).toBe('number');
+            expect(typeof refreshTokensResponse2.expiresIn).toBeGreaterThan(0);
             // upon refresh, token type given will be Bearer
-            expect(typeof refreshTokensResponse.tokenType).toEqual('Bearer');
+            expect(typeof refreshTokensResponse2.tokenType).toEqual('Bearer');
         }
 
     });
