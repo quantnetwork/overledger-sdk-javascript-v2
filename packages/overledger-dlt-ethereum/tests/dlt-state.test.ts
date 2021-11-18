5da108fd7d4622359035a9b088b87fe44e3e798b
lukerQuant <luke.riley@quant.network>
2021-11-18 13:21:44 +0000

    ethereum address search

--- packages/overledger-dlt-ethereum/tests/dlt-state.test.ts
+++ packages/overledger-dlt-ethereum/tests/dlt-state.test.ts
@@ -1,11 +1,13 @@
 import OverledgerSDK from '@quantnetwork/overledger-core';
 import { DltNameOptions } from '@quantnetwork/overledger-types';
 
-const mockAddress = '0xb619bca484d0247D88281dAF4ad62Aba666E4A8f';
-const mockPrivateKey = '0x0070999c7d2b8cf84db93ac1075ef99af3df7f59be22a7b5339fc9867c8bbf25';
 const sdkOptions = {
     dlts: [{ dlt: DltNameOptions.ETHEREUM },],
+    userPoolID: 'us-east-1_xfjNg5Nv9', //your userpool id
+    provider: { network: 'https://api.sandbox.overledger.io/v2' }
 };
+
+let address = "0x650A87cfB9165C9F4Ccc7B971D971f50f753e761";
 
 describe('Integration Tests:', () => {
 
@@ -13,25 +15,175 @@ describe('Integration Tests:', () => {
         jest.setTimeout(10000);
       });
     
-    test('Can construct the DLT', () => {
 
-        const sdk = new OverledgerSDK(sdkOptions);
-        sdk.dlts[DltNameOptions.ETHEREUM].setAccount({
-            privateKey: mockPrivateKey,
-            address: mockAddress,
-        },);
+    test('Can get address balance via prep-execute', async () => {
 
-        expect(sdk.dlts[DltNameOptions.ETHEREUM].name).toEqual(DltNameOptions.ETHEREUM);
-        expect(sdk.dlts[DltNameOptions.ETHEREUM].account.privateKey).toEqual(mockPrivateKey);
+        if (typeof process.env.USER_NAME == 'undefined') {
+            //do not run tests
+            expect(0).toEqual(0);
+            console.log("Test only to completion via automated github");
+        } else {
+            //setup sdk
+            const sdk = new OverledgerSDK(sdkOptions);
+            //get required token if secrets are available
+            const refreshTokensResponse = await sdk.getTokensUsingClientIdAndSecret(process.env.USER_NAME, process.env.PASSWORD,
+                process.env.CLIENT_ID, process.env.CLIENT_SECRET);   
+                const overledgerInstance = sdk.provider.createRequest(refreshTokensResponse.accessToken.toString()); 
+
+            //setup overledger preparation request
+            const overledgerRequest = {
+                "location": {
+                    "technology": "Ethereum",
+                    "network": "Ropsten Testnet"
+                }
+            }
+            const overledgerResponse = await overledgerInstance.post("/preparation/search/address/balance/" + address,overledgerRequest);
+            //typeof checks
+            expect(typeof overledgerResponse.data.requestId).toBe('string');
+            expect(typeof overledgerResponse.data.gatewayFee.amount).toBe('string');
+            expect(typeof overledgerResponse.data.gatewayFee.unit).toBe('string');
+            //data checks
+            expect(overledgerResponse.data.requestId.length).toBeGreaterThan(20);
+            expect(overledgerResponse.data.gatewayFee.amount.length).toBeGreaterThan(0);
+            expect(parseInt(overledgerResponse.data.gatewayFee.amount)).toBeGreaterThanOrEqual(0);
+            expect(overledgerResponse.data.gatewayFee.unit.length).toBeGreaterThan(2);
+
+            const overledgerResponse2 = await overledgerInstance.post("/execution/search/address/balance?requestId="+overledgerResponse.data.requestId);
+            //typeof checks
+            expect(typeof overledgerResponse2.data.location.technology).toBe('string');
+            expect(typeof overledgerResponse2.data.location.network).toBe('string');
+            expect(typeof overledgerResponse2.data.balances[0].unit).toBe('string');
+            //balances.value instead of balances.amount
+            //addressId is in the wrong place
+            expect(overledgerResponse2.data.location.technology).toEqual('Ethereum');
+            expect(overledgerResponse2.data.location.network).toEqual('ropsten testnet');
+            expect(overledgerResponse2.data.balances[0].unit).toEqual('ETH');
+            //balances.value instead of balances.amount
+            //addressId is in the wrong place
+        }
+
     });
 
-    test('Can get address balance', async () => {
+    test('Can get address sequence via prep-execute', async () => {
+
+        if (typeof process.env.USER_NAME == 'undefined') {
+            //do not run tests
+            expect(0).toEqual(0);
+            console.log("Test only to completion via automated github");
+        } else {
+            //setup sdk
+            const sdk = new OverledgerSDK(sdkOptions);
+            //get required token if secrets are available
+            const refreshTokensResponse = await sdk.getTokensUsingClientIdAndSecret(process.env.USER_NAME, process.env.PASSWORD,
+                process.env.CLIENT_ID, process.env.CLIENT_SECRET);   
+                const overledgerInstance = sdk.provider.createRequest(refreshTokensResponse.accessToken.toString()); 
+
+            //setup overledger preparation request
+            const overledgerRequest = {
+                "location": {
+                    "technology": "Ethereum",
+                    "network": "Ropsten Testnet"
+                }
+            }
+            const overledgerResponse = await overledgerInstance.post("/preparation/search/address/sequence/" + address,overledgerRequest);
+            //typeof checks
+            expect(typeof overledgerResponse.data.requestId).toBe('string');
+            expect(typeof overledgerResponse.data.gatewayFee.amount).toBe('string');
+            expect(typeof overledgerResponse.data.gatewayFee.unit).toBe('string');
+            //data checks
+            expect(overledgerResponse.data.requestId.length).toBeGreaterThan(20);
+            expect(overledgerResponse.data.gatewayFee.amount.length).toBeGreaterThan(0);
+            expect(parseInt(overledgerResponse.data.gatewayFee.amount)).toBeGreaterThanOrEqual(0);
+            expect(overledgerResponse.data.gatewayFee.unit.length).toBeGreaterThan(2);
+
+            const overledgerResponse2 = await overledgerInstance.post("/execution/search/block?requestId="+overledgerResponse.data.requestId);
+            //typeof checks
+                //typeof checks
+                expect(typeof overledgerResponse2.data.location.technology).toBe('string');
+                expect(typeof overledgerResponse2.data.location.network).toBe('string');
+                expect(typeof overledgerResponse2.data.sequence).toBe('string');
+                expect(typeof overledgerResponse2.data.addressId).toBe('string');
+            //data checks
+                expect(overledgerResponse2.data.location.technology).toEqual('Ethereum');
+                expect(overledgerResponse2.data.location.network).toEqual('ropsten testnet');
+                expect(parseInt(overledgerResponse2.data.sequence)).toBeGreaterThan(0);
+                expect(overledgerResponse2.data.addressId.length).toEqual(42);      
+        }
+    });
+
+
+    test('Can get address balance via autoexecute', async () => {
+
+        if (typeof process.env.USER_NAME == 'undefined') {
+            //do not run tests
+            expect(0).toEqual(0);
+            console.log("Test only to completion via automated github");
+        } else {
+            //setup sdk
+            const sdk = new OverledgerSDK(sdkOptions);
+            //get required token if secrets are available
+            const refreshTokensResponse = await sdk.getTokensUsingClientIdAndSecret(process.env.USER_NAME, process.env.PASSWORD,
+                process.env.CLIENT_ID, process.env.CLIENT_SECRET);   
+                const overledgerInstance = sdk.provider.createRequest(refreshTokensResponse.accessToken.toString()); 
+
+            //setup overledger preparation request
+            const overledgerRequest = {
+                "location": {
+                    "technology": "Ethereum",
+                    "network": "Ropsten Testnet"
+                }
+            }
+            const overledgerResponse2 = await overledgerInstance.post("/autoexecution/search/address/balance/" + address,overledgerRequest);
+            //typeof checks
+            expect(typeof overledgerResponse2.data.location.technology).toBe('string');
+            expect(typeof overledgerResponse2.data.location.network).toBe('string');
+            expect(typeof overledgerResponse2.data.balances[0].unit).toBe('string');
+            //balances.value instead of balances.amount
+            //addressId is in the wrong place
+            expect(overledgerResponse2.data.location.technology).toEqual('Ethereum');
+            expect(overledgerResponse2.data.location.network).toEqual('ropsten testnet');
+            expect(overledgerResponse2.data.balances[0].unit).toEqual('ETH');
+            //balances.value instead of balances.amount
+            //addressId is in the wrong place
+        }
 
     });
 
-    test('Can get address sequence', async () => {
+    test('Can get address sequence via autoexecute', async () => {
 
+        if (typeof process.env.USER_NAME == 'undefined') {
+            //do not run tests
+            expect(0).toEqual(0);
+            console.log("Test only to completion via automated github");
+        } else {
+            //setup sdk
+            const sdk = new OverledgerSDK(sdkOptions);
+            //get required token if secrets are available
+            const refreshTokensResponse = await sdk.getTokensUsingClientIdAndSecret(process.env.USER_NAME, process.env.PASSWORD,
+                process.env.CLIENT_ID, process.env.CLIENT_SECRET);   
+                const overledgerInstance = sdk.provider.createRequest(refreshTokensResponse.accessToken.toString()); 
+
+            //setup overledger preparation request
+            const overledgerRequest = {
+                "location": {
+                    "technology": "Ethereum",
+                    "network": "Ropsten Testnet"
+                }
+            }
+            const overledgerResponse2 = await overledgerInstance.post("/autoexecution/search/address/sequence/" + address,overledgerRequest);
+                //typeof checks
+                expect(typeof overledgerResponse2.data.location.technology).toBe('string');
+                expect(typeof overledgerResponse2.data.location.network).toBe('string');
+                expect(typeof overledgerResponse2.data.sequence).toBe('string');
+                expect(typeof overledgerResponse2.data.addressId).toBe('string');
+            //data checks
+                expect(overledgerResponse2.data.location.technology).toEqual('Ethereum');
+                expect(overledgerResponse2.data.location.network).toEqual('ropsten testnet');
+                expect(parseInt(overledgerResponse2.data.sequence)).toBeGreaterThan(0);
+                expect(overledgerResponse2.data.addressId.length).toEqual(42);      
+        }
     });
+
 
 
 });
